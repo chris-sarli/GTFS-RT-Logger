0af565ef32196c70216d831cd686d3c52fd958a7
Chris Sarli <chrismsarli@gmail.com>
2020-09-17 16:35:46 +0000

    Basic Handling of Vehicle Positions

--- gtfs-rt-logger/src/index.js
+++ gtfs-rt-logger/src/index.js
@@ -1,12 +1,11 @@
 const { Client } = require('pg');
-// const { protobuf } = require('protobufjs');
 const fetch = require('node-fetch');
 const fs = require('fs');
-// var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
-const url = process.env.GTFS_RT_URL;
+const positions_url = process.env.GTFS_RT_URL_VEHICLEPOSITIONS;
+const updates_url = process.env.GTFS_RT_URL_TRIPUPDATES;
 var protobuf = require("protobufjs");
-// console.log(protobuf);
-// 
+
+
 // details = {
 // 	user: process.env.POSTGRES_USER,
 // 	host: process.env.POSTGRES_HOST,
@@ -24,16 +23,26 @@ var protobuf = require("protobufjs");
 // })
 
 function log_rt() {
-	fetch(url).then(res => res.buffer()).then(body => {
+	const fetch_positions = fetch(positions_url)
+	const updates_positions = fetch(updates_url)
+	
+	fetch_positions.then(res => res.buffer()).then(body => {
 		protobuf.load("static/gtfs-realtime.proto")
 		.then(function(root) {
 		   var FeedMessage = root.lookupType("transit_realtime.FeedMessage")
 		   
 		   var message = FeedMessage.decode(body);
 		   var entity = message.entity;
-		   // console.log(entity);
+		   
 		   for (item in entity) {
-			   console.log(entity[item]);
+			   const vehicle = entity[item].vehicle;
+			   const trip = vehicle.trip;
+			   const position = vehicle.position;
+			   const currentStatus = vehicle.currentStatus;
+			   const timestamp = vehicle.timestamp.low;
+			   const stopId = vehicle.stopId;
+			   const vehicleId = vehicle.vehicle.id;
+			   console.log(`${timestamp}: Bus ${vehicleId} @ Stop ${stopId} (${currentStatus}) on ${trip.tripId} (${trip.routeId}), ${position.latitude}, ${position.longitude}, bearing ${position.bearing}, ${position.speed}mph`);
 		   }
 		   
 		});
